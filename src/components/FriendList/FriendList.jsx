a2fcfb3d6812333d98fe7c1a9e680f18d72d4a29
Olena Oleksandrova <ha240886kev@gmail.com>
2024-10-23 22:38:46 +0300

    friendlist

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,38 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
 
+// const FriendList = ({ friends }) => {
+//   return (
+//     <ul>
+//       {friends.map(friend => (
+//         <li key={friend.id}>
+//           <FriendListItem
+//             avatar={friend.avatar}
+//             name={friend.name}
+//             isOnline={friend.isOnline}
+//           />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
 const FriendList = ({ friends }) => {
-  return (
-    <ul>
-      {friends.map(friend => (
-        <li key={friend.id}>
-          <FriendListItem 
-            avatar={friend.avatar} 
-            name={friend.name} 
-            isOnline={friend.isOnline} 
-          />
-        </li>
-      ))}
-    </ul>
-  );
-};
+  const friendItems = [];
+
+  // Звичайний цикл для створення елементів списку
+  for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    friendItems.push(
+      <li>
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      </li>
+    );
+  }
+ return <ul>{friendItems}</ul>;
+}
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,7 +5,7 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p className={statusClass}>{isOnline ? "Online" : "Offline"}</p>
+      <p> {statusClass}</p>
     </div>
   );
 };
